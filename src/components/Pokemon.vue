56eeba029a0c5e651051e35780e9e0570ead33de
bennykoval <bethanykoval@gmail.com>
2019-08-28 22:51:01 -0400

    Get random sprite

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,6 +4,7 @@
             <h1>ayyylmao</h1>
             <button
             @click='getPokemon(getRandomInt())'>touch me</button>
+            <img :src="frontSprite"/>
         </div>
     </main>
 </template>
@@ -12,14 +13,34 @@
 import axios from "axios";
 
 export default {
-  name: 'HelloWorld',
+  name: 'Pokemon',
+  data: async () => {
+      return {
+          frontSprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/173.png"
+      }
+  },
+  async mounted() {
+    //   let frontSprite = ''
+      await this.getPokemon(this.getRandomInt());
+  },
   methods: { 
       getRandomInt() {
+          console.log('hi', this.frontSprite)
           return Math.floor(Math.random() * Math.floor(251))
       },
       getPokemon : async(id) => {
+        // console.log('async', this.frontSprite)
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        console.log(response.data.name)
+        await console.log(response.data.sprites.front_default)
+        this.frontSprite = await response.data.sprites.front_default
+        console.log('typeof front', typeof(frontSprite))
+        // return frontSprite
+        // console.log(this.frontSprite)
+      },
+      anothaOne(frontURL) {
+          console.log('typeof front in anotha', typeof(frontURL))
+          this.frontSprite = frontURL
+          console.log('anothaone', this.frontSprite)
       }
   }
 }
