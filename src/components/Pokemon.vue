9da7f3c22c89ab13f3347faed2306df2033d4c0f
bennykoval <bethanykoval@gmail.com>
2019-08-28 21:48:52 -0400

    Randomly generate pokemon names Co-authored-by: Zeke Hart heirloch@gmail.com

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div>
+            <h1>ayyylmao</h1>
+            <button
+            @click='getPokemon(getRandomInt())'>touch me</button>
+        </div>
+    </main>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'HelloWorld',
+  methods: { 
+      getRandomInt() {
+          return Math.floor(Math.random() * Math.floor(251))
+      },
+      getPokemon : async(id) => {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        console.log(response.data.name)
+      }
+  }
+}
+</script>
